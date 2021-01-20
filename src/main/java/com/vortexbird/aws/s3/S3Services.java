5d507e6b7abd9a5b394aef1ec20fbe653a60a646
Diego Armando Gomez <diegomezusb@gmail.com>
2021-01-20 17:03:01 -0500

    Carga inicial

---
+++ src/main/java/com/vortexbird/aws/s3/S3Services.java
@@ -0,0 +1,26 @@
+package com.vortexbird.aws.s3;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+
+public interface S3Services {
+
+	public void createBucket(String bucketName)throws Exception;
+	
+	public String uploadPrivate(String bucketName,String key, File file )throws Exception;
+	
+	public String uploadPublicRead(String bucketName,String key, File file )throws Exception;
+	
+	public List<String> listBucketsName()throws Exception;
+	 
+	public InputStream download(String bucketName, String key)throws Exception;
+	 
+	public void deleteObject(String bucketName, String key)throws Exception;
+	 
+	public void deleteBucket(String bucketName)throws Exception;
+	
+	public String getUrl(String bucketName, String key)throws Exception;
+
+}
